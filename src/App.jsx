1c06a6959e131ab41410f92a051097af1dc92eb4
Ranjeet550 <ry151916@gmail.com>
2025-10-05 17:00:52 +0530

    fix hosting issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
 
-export default function App() {
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home.jsx';
+import Courses from './pages/Courses.jsx';
+import Blog from './pages/Blog.jsx';
+import About from './pages/About.jsx';
+import Team from './pages/Team.jsx';
+import Contact from './pages/Contact.jsx';
+
+function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
@@ -12,5 +18,25 @@ export default function App() {
       </div>
       <Footer />
     </div>
-  )
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />, // Layout
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'courses', element: <Courses /> },
+      { path: 'blog', element: <Blog /> },
+      { path: 'about', element: <About /> },
+      { path: 'team', element: <Team /> },
+      { path: 'contact', element: <Contact /> },
+    ],
+  },
+]);
+
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
